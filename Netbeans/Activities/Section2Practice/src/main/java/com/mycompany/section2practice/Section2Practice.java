0192eee996595d391e3ef11fc4c611263cb3a8e3
ckxsl023 <70303889+ckxsl023@users.noreply.github.com>
2022-05-25 14:54:27 +0800

    NetBeans Projects

---
+++ Netbeans/Activities/Section2Practice/src/main/java/com/mycompany/section2practice/Section2Practice.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.section2practice;
+
+/**
+ *
+ * @author ckxsl023
+*/
+
+
+public class Section2Practice {
+    public static void drawAsciiArt(){
+        System.out.println("--____--");
+        System.out.println("-|____|-");
+        System.out.println("|_X__X_|");
+        System.out.println("|______|");
+        System.out.println("|_X__X_|");
+        System.out.println("|__XX__|");
+        System.out.println("-|____|-");
+        System.out.println("--------");
+    }
+    
+    public static void drawArt(){ //Draws A Grim Reaper
+        System.out.println("             ___          ");
+        System.out.println("            /   \\        ");
+        System.out.println("      ////\\|0  0 ||      ");
+        System.out.println("   ////   \\ ___//\       ");
+        System.out.println("  ///      \\      \      ");
+        System.out.println(" ///       |\\      |     ");
+        System.out.println("//         | \\  \   \    ");
+        System.out.println("/          |  \\  \   \   ");
+        System.out.println("           |   \\ /   /   ");
+        System.out.println("           |    \/   /    ");
+        System.out.println("           |     \\/|     ");
+        System.out.println("           |      \\|     ");
+        System.out.println("           |        |     ");
+        System.out.println("           |_________\    ");
+    
+}
+
+public static void main(String[] args) {
+
+        drawAsciiArt();
+        drawArt();
+    }    
+}
+
